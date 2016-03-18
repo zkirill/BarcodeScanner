57bb65f9e4d9bd7a0cfafc2313c177106fb3919a
Vadym Markov <markov.vadym@gmail.com>
2016-03-18 14:45:43 +0100

    handle orientation

--- Sources/BarcodeScannerController.swift
+++ Sources/BarcodeScannerController.swift
@@ -3,14 +3,11 @@ import AVFoundation
 
 public class BarcodeScannerController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
 
-  lazy var messageLabel: UILabel = {
+  lazy var infoView: UILabel = {
     let label = UILabel()
     label.backgroundColor = .whiteColor()
     return label
   }()
-
-  lazy var videoView: UIView = UIView()
-
 
   private var readableCodeTypes = [
     AVMetadataObjectTypeUPCECode,
@@ -25,9 +22,43 @@ public class BarcodeScannerController: UIViewController, AVCaptureMetadataOutput
     AVMetadataObjectTypeAztecCode
   ]
 
+  var infoViewFrame: CGRect {
+    let height = view.frame.height / 3
+
+    return CGRect(x: 0, y: view.bounds.height - height,
+      width: view.bounds.width, height: height)
+  }
+
   var captureSession: AVCaptureSession?
   var videoPreviewLayer: AVCaptureVideoPreviewLayer?
   var focusView: UIView?
   var captureDevice: AVCaptureDevice?
   var capturedCode:String?
+
+  // MARK: - View lifecycle
+
+  public override func viewDidLoad() {
+    super.viewDidLoad()
+
+    view.addSubview(infoView)
+  }
+
+  // MARK: - Layout
+
+  public override func viewWillLayoutSubviews() {
+    super.viewWillLayoutSubviews()
+
+    infoView.frame = infoViewFrame
+    videoPreviewLayer?.frame = view.layer.bounds
+
+    let orientation = UIApplication.sharedApplication().statusBarOrientation
+    videoPreviewLayer?.connection.videoOrientation = orientation.captureOrientation
+  }
+
+  // MARK: - Orientation
+
+  public override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+    super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
+    videoPreviewLayer?.frame.size = size
+  }
 }

60da5f177152832a4f66f88b3f9bc4b7aa007a45
Carl Brusell <carlbr@me.com>
2017-07-13 13:35:32 -0300

    remove not useful comment and newlines before and after cases

--- Sources/FocusViewType.swift
+++ Sources/FocusViewType.swift
@@ -1,17 +1,7 @@
-//
-//  FocusViewType.swift
-//  BarcodeScanner
-//
-//  Created by Carl Brusell on 12/07/17.
-//  Copyright © 2017 Hyper Interaktiv AS. All rights reserved.
-//
-
 import UIKit
 
 public enum FocusViewType {
-    
     case animated
     case oneDimension
     case twoDimensions
-
 }

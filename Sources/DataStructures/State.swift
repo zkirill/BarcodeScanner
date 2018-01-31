a6c81362487aa8eaf67ac8e8146fa75321dc2ed3
Vadym Markov <markov.vadym@gmail.com>
2018-01-31 21:15:50 +0100

    Implement state message provider to be able to set custom info messages

--- Sources/DataStructures/State.swift
+++ Sources/DataStructures/State.swift
@@ -1,5 +1,36 @@
 import UIKit
 
+// MARK: - State
+
+/// Barcode scanner state.
+enum State {
+  case scanning
+  case processing
+  case unauthorized
+  case notFound
+}
+
+/// State message provider.
+public struct StateMessageProvider {
+  public var scanningText = localizedString("INFO_DESCRIPTION_TEXT")
+  public var processingText = localizedString("INFO_LOADING_TITLE")
+  public var unathorizedText = localizedString("ASK_FOR_PERMISSION_TEXT")
+  public var notFoundText = localizedString("NO_PRODUCT_ERROR_TITLE")
+
+  func makeText(for state: State) -> String {
+    switch state {
+    case .scanning:
+      return scanningText
+    case .processing:
+      return processingText
+    case .unauthorized:
+      return unathorizedText
+    case .notFound:
+      return notFoundText
+    }
+  }
+}
+
 // MARK: - Status
 
 /// Status is a holder of the current state with a few additional configuration properties.
@@ -9,7 +40,7 @@ struct Status {
   /// Flag to enable/disable animation.
   let animated: Bool
   /// Text that overrides a text from the state.
-  let text: String
+  let text: String?
 
   /**
    Creates a new instance of `Status`.
@@ -20,30 +51,6 @@ struct Status {
   init(state: State, animated: Bool = true, text: String? = nil) {
     self.state = state
     self.animated = animated
-    self.text = text ?? state.text
-  }
-}
-
-// MARK: - State
-
-/// Barcode scanner state.
-enum State {
-  case scanning
-  case processing
-  case unauthorized
-  case notFound
-
-  /// State message.
-  var text: String {
-    switch self {
-    case .scanning:
-      return localizedString("INFO_DESCRIPTION_TEXT")
-    case .processing:
-      return localizedString("INFO_LOADING_TITLE")
-    case .unauthorized:
-      return localizedString("ASK_FOR_PERMISSION_TEXT")
-    case .notFound:
-      return localizedString("NO_PRODUCT_ERROR_TITLE")
-    }
+    self.text = text
   }
 }
